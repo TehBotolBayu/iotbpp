ec5e0de9a60c814b2f218e55c0a1ef290f634aff
tehbotolbayu <bayuuabdur2903@gmail.com>
2024-02-16 21:22:43 +0800

    init

---
+++ js/const.js
@@ -0,0 +1,31 @@
+const data = {
+    heroTitle: "Welcome to SafeGuard: Your Premier IoT Fire Hazard Detector Solution",
+    heroDes: "Are you concerned about the safety of your home or workplace? Look no further! SafeGuard offers cutting-edge IoT fire hazard detectors designed to protect what matters most to you.",
+    whyTitle: "Why Choose FireX?",
+    whyList: [
+        {
+            title: "Advanced Detection Technology",
+            des: "Our IoT fire hazard detectors utilize state-of-the-art sensors and algorithms to detect potential fire hazards before they escalate, providing you with early warnings and peace of mind.",
+        },
+        {
+            title: "24/7 Monitoring",
+            des: "With SafeGuard, you're never alone in safeguarding your property. Our detectors are constantly monitoring for signs of danger, even when you're away, ensuring round-the-clock protection.",
+        },
+        {
+            title: "Instant Alerts",
+            des: "Receive instant notifications on your smartphone or tablet at the first sign of smoke or increased temperature levels. Time is critical in fire emergencies, and our rapid alert system ensures swift action can be taken to mitigate risks."
+        },
+        {
+            title: "Easy Integration",
+            des: "SafeGuard seamlessly integrates into your smart home or office ecosystem, allowing you to control and monitor your detectors from anywhere with an internet connection. Compatibility with leading smart home platforms makes setup a breeze.",
+        },
+        {
+            title: "Reliable Performance",
+            des: "Built with durability and reliability in mind, our IoT fire hazard detectors are engineered to withstand the test of time, offering long-term protection for your property and loved ones."
+        }
+    ],
+    endNote: {
+        title: "Experience the Future of Fire Safety",
+        des: "At SafeGuard, we're committed to revolutionizing fire safety through innovation and technology. Don't wait until it's too late – invest in the future of protection with SafeGuard IoT fire hazard detectors today.",
+    }
+}
